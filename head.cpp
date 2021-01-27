736a503838081f058e4c0b52e9fd886ec5bd844e
Alamingit247 <68566244+Alamingit247@users.noreply.github.com>
2021-01-27 23:28:22 +0600

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "head.h"
+using namespace std;
+
+
+int main()
+{
+    int n1 = 10, n2 = 50, n3;
+    n3 = addd(n1, n2);
+    cout << "sum : " << n3;
+    return 0;
+}
